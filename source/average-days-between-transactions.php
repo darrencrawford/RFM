414585c7eea673c7f81fdcbdacf09b0a65c98513
Darren Crawford <darren@darrencrawford.com>
2013-12-24 17:19:26 -0500

    Fixed Average Days
    Now works

--- source/average-days-between-transactions.php
+++ source/average-days-between-transactions.php
@@ -23,40 +23,11 @@ then add a number field for 'Click Count'
 and a date field for 'Last Click Date'
 
 Setup your PING URL Like this:
-http://clickstats.co/kinostats/average-days-between-transactions.php?id=[contact_id]&transactions=[Number of Transactions]&lead_to_purchase=[Days between Lead and Purchase]&purchases1_2=[Days between Purchases 1 and 2]&purchases2_3=[Days between Purchases 2 and 3]&purchases3_4=[Days between Purchases 3 and 4]&purchases4_5=[Days between Purchases 4 and 5]
+http://clickstats.co/kinostats/average-days-between-transactions.php?id=[contact_id]&transactions=[Number of Transactions]&lead_to_purchase=[Days between Lead and Purchase]&purchases2=[Days between Purchases 1 and 2]&purchases3=[Days between Purchases 2 and 3]&purchases4=[Days between Purchases 3 and 4]&purchases5=[Days between Purchases 4 and 5]
 
 For statement.  If transactions = 1 then average = number of days.  if trans = 2 then average = (purchases1_2+purchases2_3)
 
 */
-
-
-
-//GET variables from URL
-$contact_id = $_GET["id"];
-$transactions = $_GET["transactions"];
-$lead_to_purchase = $_GET['lead_to_purchase'];
-$purchases1_2 = $_GET['purchases1_2'];
-$purchases2_3 = $_GET['purchases2_3'];
-$purchases3_4 = $_GET['purchases3_4'];
-$purchases4_5 = $_GET['purchases4_5'];
-
-
-//echo $transactions;
-
-	if ($transactions==1) {
-		$average_transaction = $lead_to_purchase;
-	} elseif ($transactions==2) {
-		$average_transaction = (($lead_to_purchase+$purchases1_2)/2);
-	} elseif ($transactions==3) {
-		$average_transaction = (($lead_to_purchase+$purchases1_2+$purchases2_3)/3);
-	} elseif ($transactions==4) {
-		$average_transaction = (($lead_to_purchase+$purchases1_2+$purchases2_3+$purchases3_4)/4);
-	} elseif ($transactions>=5) {
-		$average_transaction = (($lead_to_purchase+$purchases1_2+$purchases2_3+$purchases3_4+$purchases4_5)/5);
-	}
-
-//echo 'Avg Trans = '.$average_transaction;
-
 
 //Write the exact URL being pinged to stats_log file 
 $filename = '/home/stats/public_html/kinostats/logs/average-days-between-transactions_log';
@@ -67,6 +38,42 @@ $filename = '/home/stats/public_html/kinostats/logs/average-days-between-transac
 	$write = $logDate." - ".$addr."\n";
 	file_put_contents ( $filename ,  $write , FILE_APPEND | LOCK_EX);
 
+// /kinostats/average-days-between-transactions.php?id=3432&transactions=5&lead_to_purchase=112&purchases1_2=0&purchases2_3=0&purchases3_4=0&purchases4_5=0
+
+
+//GET variables from URL
+$contact_id = $_GET["id"];
+	//echo 'id = '.$contact_id.'<br/>';
+$transactions = $_GET["transactions"];
+	//echo 'transactions = '.$transactions.'<br/>';
+$lead_to_purchase = $_GET['lead_to_purchase'];
+	//echo 'lead to purchase = '.$lead_to_purchase.'<br/>';
+$purchases2 = $_GET['purchases2'];
+	//echo 'purchases2 = '.$purchases2.'<br/>';
+$purchases3 = $_GET['purchases3'];
+	//echo 'purchases3 = '.$purchases3.'<br/>';
+$purchases4 = $_GET['purchases4'];
+	//echo 'purchases4 = '.$purchases4.'<br/>';
+$purchases5 = $_GET['purchases5'];
+	//echo 'purchases5 = '.$purchases5.'<br/>';
+
+
+//echo $transactions;
+
+	if ($transactions==1) {
+		$average_transaction = $lead_to_purchase;
+	} elseif ($transactions==2) {
+		$average_transaction = (($lead_to_purchase+$purchases2)/2);
+	} elseif ($transactions==3) {
+		$average_transaction = (($lead_to_purchase+$purchases2+$purchases3)/3);
+	} elseif ($transactions==4) {
+		$average_transaction = (($lead_to_purchase+$purchases2+$purchases3+$purchases4)/4);
+	} elseif ($transactions>=5) {
+		$average_transaction = (($lead_to_purchase+$purchases2+$purchases3+$purchases4+$purchases5)/5);
+	}
+
+$average_transaction = round($average_transaction);
+//echo 'Avg Trans = '.$average_transaction;
 
 
 $data = <<<STRING
@@ -98,6 +105,5 @@ curl_close($session);
 header("Content-Type: text/xml");
 echo $response;
 
-*/
 ?>
 
