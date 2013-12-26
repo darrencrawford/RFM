8af5276c31e0913ae7aac11a989bf0e2dacc4a99
Darren Crawford <darren@darrencrawford.com>
2013-12-26 17:25:40 -0500

    Release Candidate 1
    RFM.One is complete and ready for testing

--- source/rfm1.php
+++ source/rfm1.php
@@ -12,11 +12,7 @@
 
 /*
 ====================================================================
-The following are the shortcodes to be called in the URL for the 
-specific function:
-
-daysbetween1-2 = Days Between Purchases 1 and 2
-
+see the README file for exact links to include in your Ontraport sequences
 
 ====================================================================
 */
@@ -31,6 +27,30 @@ include 'keys.php';
 	$contact_id = $_GET["id"];
 	$rfm = $_GET['rfm'];
 	
+
+
+function api_call($contact_id, $data, $appid, $key) {
+	
+	$data = urlencode(urlencode($data));
+
+	// Replace the strings with your API credentials located in Admin > OfficeAutoPilot API Instructions and Key Manager
+	// $appid = "2_11369_H4sKVMEiL";
+	// $key = "5VgmlzqqiY55TuI";
+
+	$reqType= "update";
+	$postargs = "appid=".$appid."&key=".$key."&return_id=1&reqType=".$reqType. "&data=" . $data;
+	$request = "https://api.moon-ray.com/cdata.php";
+
+	$session = curl_init($request);
+	curl_setopt ($session, CURLOPT_POST, true);
+	curl_setopt ($session, CURLOPT_POSTFIELDS, $postargs);
+	curl_setopt($session, CURLOPT_HEADER, false);
+	curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+	$response = curl_exec($session);
+	curl_close($session);
+	header("Content-Type: text/xml");
+	echo $response;
+}
 
 
 function write_log($logname) {
@@ -206,7 +226,6 @@ function average_days_between($contact_id, $appid, $key) {
 	$purchases4 = $_GET['purchases4'];
 	$purchases5 = $_GET['purchases5'];
 
-
 		if ($transactions==1) {
 			$average_transaction = $lead_to_purchase;
 		} elseif ($transactions==2) {
@@ -223,83 +242,193 @@ function average_days_between($contact_id, $appid, $key) {
 	//echo 'Avg Trans = '.$average_transaction;
 
 
-$data = <<<STRING
-//You must pass the contact ID as an argument to indicate which contact is being updated
-<contact id="$contact_id">
-<Group_Tag name="RFM">
-<field name="Average Days Between Purchases">$average_transaction</field>
-</Group_Tag>
-</contact>
+	$data = <<<STRING
+	//You must pass the contact ID as an argument to indicate which contact is being updated
+	<contact id="$contact_id">
+	<Group_Tag name="RFM">
+	<field name="Average Days Between Purchases">$average_transaction</field>
+	</Group_Tag>
+	</contact>
 STRING;
 
 	api_call($contact_id, $data, $appid, $key);
 }
 
 //Clickcount Goes Here
+function clickcount($contact_id, $appid, $key) {
+	
+	// Count number of clicks and update last click date
+	$clickCount = $_GET["clickcount"];
+	$lastClickDate = $_GET["lastclickdate"];
+		
+	if(empty($lastClickDate))
+			{
+				$lastClickDate = '1/1/1969';
+			}
+	$clickCount = $clickCount+1;
+	$lastClickDate = date('m/d/Y');
+	$lastClickDate = mktime($lastClickDate);
 
+	$data = <<<STRING
+	//You must pass the contact ID as an argument to indicate which contact is being updated
+	<contact id="$contact_id">
+	<Group_Tag name="RFM">
+	<field name="Total Number of Clicks">$clickCount</field>
+	<field name="Date of Last Click">$lastClickDate</field>
+	</Group_Tag>
+	</contact>
+STRING;
+	
+	//echo $clickCount.'<br>';
+	//echo $lastClickDate.'<br>';
+	api_call($contact_id, $data, $appid, $key);
 
-//Countdays
-
+}
 
 //Days Between Lead-Purchase
+function days_between_lead_purchase($contact_id, $appid, $key) {
+	//GET variables from URL
+	$contact_id = $_GET["id"];
+	$MDYDate = $_GET["dateadded"];
 
+	$pieces = explode("-", $MDYDate);	
+	$timestamp = $pieces[1].'-'.$pieces[0].'-'.$pieces[2];
+	$timestamp = strtotime($timestamp);
+	$now = time();
+	$diff = $now-$timestamp;
+	$diff = floor($diff/86400);
+	$diff_in_days = $diff;
+
+	$data = <<<STRING
+	//You must pass the contact ID as an argument to indicate which contact is being updated
+	<contact id="$contact_id">
+	<Group_Tag name="RFM">
+	<field name="Days between Lead and Purchase">$diff_in_days</field>
+	</Group_Tag>
+	</contact>
+STRING;
+
+	api_call($contact_id, $data, $appid, $key);
+}
 
 //Lifetime Value
+function lifetimevalue($contact_id, $appid, $key) {
+	//GET variables from URL
+$contact_id = $_GET["contact_id"];
+$purchases = $_GET["purchases"];
+$last_amount = $_GET["last_amount"];
+	$last_amount = substr($last_amount, 1);
+	$last_amount = str_replace(",", "", $last_amount); //remove commas from the numbers
+$lifevalue = $_GET["lifevalue"];
+	$lifevalue = substr($lifevalue, 1);
+	$lifevalue = str_replace(",", "", $lifevalue);
 
+
+//Do math - add one to current number of purchases
+$purchases=$purchases+1;
+$lifevalue = $lifevalue + $last_amount;
+$avgTrans = $lifevalue/$purchases; // do a calc to tally the average transaction value
+
+
+	$data = <<<STRING
+	//You must pass the contact ID as an argument to indicate which contact is being updated
+	<contact id="$contact_id">
+	<Group_Tag name="Buying Cycles">
+	<field name="Number of Purchases">$purchases</field>
+	<field name="Lifetime Value">$lifevalue</field>
+	<field name="Average Transaction Value">$avgTrans</field>
+	</Group_Tag>
+	</contact>
+STRING;
+	
+	api_call($contact_id, $data, $appid, $key);
+}
 
 //Name Purchase 1
+function name_purchase_1($contact_id, $appid, $key) {
+	//GET variables from URL
+	$contact_id = $_GET["id"];
+	$nameLastPurchase = $_GET["namelastpurchase"];
 
+	$data = <<<STRING
+	//You must pass the contact ID as an argument to indicate which contact is being updated
+	<contact id="$contact_id">
+	<Group_Tag name="RFM">
+	<field name="Name of Purchase 1">$nameLastPurchase</field>
+	</Group_Tag>
+	</contact>
+STRING;
+
+	api_call($contact_id, $data, $appid, $key);
+}
 
 //Name Purchase 2
+function name_purchase_2($contact_id, $appid, $key) {
+	//GET variables from URL
+	$contact_id = $_GET["id"];
+	$nameLastPurchase = $_GET["namelastpurchase"];
 
+	$data = <<<STRING
+	//You must pass the contact ID as an argument to indicate which contact is being updated
+	<contact id="$contact_id">
+	<Group_Tag name="RFM">
+	<field name="Name of Purchase 2">$nameLastPurchase</field>
+	</Group_Tag>
+	</contact>
+STRING;
+
+	api_call($contact_id, $data, $appid, $key);
+}
 
 //Name Purchase 3
+function name_purchase_3($contact_id, $appid, $key) {
+	//GET variables from URL
+	$contact_id = $_GET["id"];
+	$nameLastPurchase = $_GET["namelastpurchase"];
 
+	$data = <<<STRING
+	//You must pass the contact ID as an argument to indicate which contact is being updated
+	<contact id="$contact_id">
+	<Group_Tag name="RFM">
+	<field name="Name of Purchase 3">$nameLastPurchase</field>
+	</Group_Tag>
+	</contact>
+STRING;
 
-//Purchases
+	api_call($contact_id, $data, $appid, $key);
+}
 
-
-//Score
 
 
 //Transaction Count
+function transaction_count($contact_id, $appid, $key) {
+	//GET variables from URL
+	$contact_id = $_GET["id"];
+	$oldCount = $_GET["transactions"];
+	$last_amount = $_GET["last_amount"];
+		$last_amount = substr($last_amount, 1); //remove $ sign
+		$last_amount = str_replace(",", "", $last_amount); //remove commas from the numbers
+	$totalSpent = $_GET["spent"];
+		$totalSpent = substr($totalSpent, 1);
+		$totalSpent = str_replace(",", "", $totalSpent);	
 
 
+	//Do math - add one to current number of purchases
+	$newCount = $oldCount+1;
+	$newSpent = $totalSpent + $last_amount;
 
 
+	$data = <<<STRING
+	//You must pass the contact ID as an argument to indicate which contact is being updated
+	<contact id="$contact_id">
+	<Group_Tag name="RFM">
+	<field name="Number of Transactions">$newCount</field>
+	<field name="Total Spent">$newSpent</field>
+	</Group_Tag>
+	</contact>
+STRING;
 
-
-
-
-
-
-
-
-
-
-
-
-function api_call($contact_id, $data, $appid, $key) {
-	
-	$data = urlencode(urlencode($data));
-
-	// Replace the strings with your API credentials located in Admin > OfficeAutoPilot API Instructions and Key Manager
-	// $appid = "2_11369_H4sKVMEiL";
-	// $key = "5VgmlzqqiY55TuI";
-
-	$reqType= "update";
-	$postargs = "appid=".$appid."&key=".$key."&return_id=1&reqType=".$reqType. "&data=" . $data;
-	$request = "https://api.moon-ray.com/cdata.php";
-
-	$session = curl_init($request);
-	curl_setopt ($session, CURLOPT_POST, true);
-	curl_setopt ($session, CURLOPT_POSTFIELDS, $postargs);
-	curl_setopt($session, CURLOPT_HEADER, false);
-	curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
-	$response = curl_exec($session);
-	curl_close($session);
-	header("Content-Type: text/xml");
-	echo $response;
+	api_call($contact_id, $data, $appid, $key);
 }
 
 
@@ -322,13 +451,24 @@ if ($rfm == 'daysbetween1-2') {
 		days_between_last_now($contact_id, $date_purchase, $appid, $key);
 	} elseif ($rfm == 'averagedaysbetween') {
 		average_days_between($contact_id, $appid, $key);
+	} elseif ($rfm == 'clickcounter') {
+		clickcount($contact_id, $appid, $key);
+	} elseif ($rfm == 'daysbetweenleadpurchase') {
+		days_between_lead_purchase($contact_id, $appid, $key);
+	} elseif ($rfm == 'lifetimevalue') {
+		lifetimevalue($contact_id, $appid, $key);
+	} elseif ($rfm == 'namepurchase1') {
+		name_purchase_1($contact_id, $appid, $key);
+	} elseif ($rfm == 'namepurchase2') {
+		name_purchase_2($contact_id, $appid, $key);
+	} elseif ($rfm == 'namepurchase3') {
+		name_purchase_3($contact_id, $appid, $key);
+	} elseif ($rfm == 'transactioncount') {
+		transaction_count($contact_id, $appid, $key);
 	}
-
-
 		else {
-		echo "Monkey Buns!";
+		echo "Monkey Buns! (AKA Error!)";
 	}
-
 
 
 ?>
