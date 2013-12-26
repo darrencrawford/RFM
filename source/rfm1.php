732b15cfc928b4719d02560e9add3f49c16c4eaf
Darren Crawford <darren@darrencrawford.com>
2013-12-25 22:27:22 -0500

    5 Functions Working

--- source/rfm1.php
+++ source/rfm1.php
@@ -10,6 +10,19 @@
  */
 
 
+/*
+====================================================================
+The following are the shortcodes to be called in the URL for the 
+specific function:
+
+daysbetween1-2 = Days Between Purchases 1 and 2
+
+
+====================================================================
+*/
+
+
+
 //Get appid and key for OAP
 include 'keys.php';
 	//echo $appid;
@@ -17,7 +30,7 @@ include 'keys.php';
 //Get specific variables to determine which function to call
 	$contact_id = $_GET["id"];
 	$rfm = $_GET['rfm'];
-	$date_purchase = $_GET["datepurchase"];
+	
 
 
 function write_log($logname) {
@@ -32,7 +45,7 @@ function write_log($logname) {
 }
 
 
-function days_between_1($contact_id, $date_purchase, $appid, $key) {
+function days_between_1_2($contact_id, $date_purchase, $appid, $key) {
 	
 	//convert date to D-M-Y format
 		$pieces = explode("-", $date_purchase);	
@@ -55,9 +68,173 @@ function days_between_1($contact_id, $date_purchase, $appid, $key) {
 	</contact>
 STRING;
 
-	return $data; // send the data string back for the api_call function
+	//return $data; // send the data string back for the api_call function
 	//return $logname; // send the logname back to be written
+
+	api_call($contact_id, $data, $appid, $key);
+	//write_log($logname); // write_log is for debugging
 }
+
+function days_between_2_3($contact_id, $date_purchase, $appid, $key) {
+	
+	//convert date to D-M-Y format
+		$pieces = explode("-", $date_purchase);	
+		$timestamp = $pieces[1].'-'.$pieces[0].'-'.$pieces[2];
+		$timestamp = strtotime($timestamp);
+		$now = time();
+		$diff = $now-$timestamp;
+		$diff = floor($diff/86400);
+		$diff_in_days = $diff;
+		$logname = 'days-between-2-3_log';
+
+	//echo 'diff in function = '.$diff_in_days.'<br>';
+
+	$data = <<<STRING
+	//You must pass the contact ID as an argument to indicate which contact is being updated
+	<contact id="$contact_id">
+	<Group_Tag name="RFM">
+	<field name="Days between Purchases 2 and 3">$diff_in_days</field>
+	</Group_Tag>
+	</contact>
+STRING;
+
+	//return $data; // send the data string back for the api_call function
+	//return $logname; // send the logname back to be written
+
+	api_call($contact_id, $data, $appid, $key);
+	//write_log($logname); // write_log is for debugging
+}
+
+function days_between_3_4($contact_id, $date_purchase, $appid, $key) {
+	
+	//convert date to D-M-Y format
+		$pieces = explode("-", $date_purchase);	
+		$timestamp = $pieces[1].'-'.$pieces[0].'-'.$pieces[2];
+		$timestamp = strtotime($timestamp);
+		$now = time();
+		$diff = $now-$timestamp;
+		$diff = floor($diff/86400);
+		$diff_in_days = $diff;
+		$logname = 'days-between-3-4_log';
+
+	//echo 'diff in function = '.$diff_in_days.'<br>';
+
+	$data = <<<STRING
+	//You must pass the contact ID as an argument to indicate which contact is being updated
+	<contact id="$contact_id">
+	<Group_Tag name="RFM">
+	<field name="Days between Purchases 3 and 4">$diff_in_days</field>
+	</Group_Tag>
+	</contact>
+STRING;
+
+	//return $data; // send the data string back for the api_call function
+	//return $logname; // send the logname back to be written
+
+	api_call($contact_id, $data, $appid, $key);
+	//write_log($logname); // write_log is for debugging
+}
+
+function days_between_4_5($contact_id, $date_purchase, $appid, $key) {
+	
+	//convert date to D-M-Y format
+		$pieces = explode("-", $date_purchase);	
+		$timestamp = $pieces[1].'-'.$pieces[0].'-'.$pieces[2];
+		$timestamp = strtotime($timestamp);
+		$now = time();
+		$diff = $now-$timestamp;
+		$diff = floor($diff/86400);
+		$diff_in_days = $diff;
+		$logname = 'days-between-4-5_log';
+
+	//echo 'diff in function = '.$diff_in_days.'<br>';
+
+	$data = <<<STRING
+	//You must pass the contact ID as an argument to indicate which contact is being updated
+	<contact id="$contact_id">
+	<Group_Tag name="RFM">
+	<field name="Days between Purchases 4 and 5">$diff_in_days</field>
+	</Group_Tag>
+	</contact>
+STRING;
+
+	//return $data; // send the data string back for the api_call function
+	//return $logname; // send the logname back to be written
+
+	api_call($contact_id, $data, $appid, $key);
+	//write_log($logname); // write_log is for debugging
+}
+
+function days_between_last_now($contact_id, $date_purchase, $appid, $key) {
+	//convert date to D-M-Y format
+	$pieces = explode("-", $date_purchase);	
+	$timestamp = $pieces[1].'-'.$pieces[0].'-'.$pieces[2];
+	$temp = $timestamp;
+	$timestamp = strtotime($timestamp);
+	$now = time();
+	$diff = $now-$timestamp;
+	$diff = floor($diff/86400);
+	$diff_in_days = $diff;
+	$dateLastPurchase = time();
+
+	$data = <<<STRING
+	//You must pass the contact ID as an argument to indicate which contact is being updated
+	<contact id="$contact_id">
+	<Group_Tag name="RFM">
+	<field name="Date of Last Purchase">$dateLastPurchase</field>
+	<field name="Days since Last Purchase">$diff_in_days</field>
+	</Group_Tag>
+	</contact>
+STRING;
+
+	api_call($contact_id, $data, $appid, $key);
+}
+
+//Average days between transactions
+
+//Clickcount Goes Here
+
+
+//Countdays
+
+
+//Days Between Lead-Purchase
+
+
+//Lifetime Value
+
+
+//Name Purchase 1
+
+
+//Name Purchase 2
+
+
+//Name Purchase 3
+
+
+//Purchases
+
+
+//Score
+
+
+//Transaction Count
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function api_call($contact_id, $data, $appid, $key) {
 	
@@ -83,13 +260,28 @@ function api_call($contact_id, $data, $appid, $key) {
 }
 
 
-if ($rfm = 'datepurchase1') {
-	$data = days_between_1($contact_id, $date_purchase, $appid, $key);		
-	sleep(1);
-	api_call($contact_id, $data, $appid, $key);
-	write_log($logname);
-	} else {
-		echo 'No Match';
+if ($rfm == 'daysbetween1-2') {
+	$date_purchase = $_GET["datepurchase"];
+	days_between_1_2($contact_id, $date_purchase, $appid, $key);		
+	
+	
+	} elseif ($rfm == 'daysbetween2-3') {
+		$date_purchase = $_GET["datepurchase"];
+		days_between_2_3($contact_id, $date_purchase, $appid, $key);
+	} elseif ($rfm == 'daysbetween3-4') {
+		$date_purchase = $_GET["datepurchase"];
+		days_between_3_4($contact_id, $date_purchase, $appid, $key);
+	} elseif ($rfm == 'daysbetween4-5') {
+		$date_purchase = $_GET["datepurchase"];
+		days_between_4_5($contact_id, $date_purchase, $appid, $key);
+	} elseif ($rfm == 'daysbetweenlastnow') {
+		$date_purchase = $_GET["datepurchase"];
+		days_between_last_now($contact_id, $date_purchase, $appid, $key);
+	}
+
+
+		else {
+		echo "Monkey Buns!";
 	}
 
 
