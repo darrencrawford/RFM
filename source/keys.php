b6d6f7f2476e0ec5a6e6a25c3a749e2ac1306fc3
Darren Crawford <darren@darrencrawford.com>
2013-12-25 20:15:55 -0500

    RFM1 testing
    working as a single function. Need to expand to discrete functions

---
+++ source/keys.php
@@ -0,0 +1,7 @@
+<?php
+// Set your OAP / Ontraport API ID & Key here.  Make sure they have read/write/update access.  Delete access is not needed.
+
+	$appid = "2_11369_H4sKVMEiL";
+	$key = "5VgmlzqqiY55TuI";
+
+?>
--- source/rfm1.php
+++ source/rfm1.php
@@ -9,15 +9,46 @@
  *	License: GPL2
  */
 
+/* 
+ * To-Do
+ * 
+*/
+
+//Get appid and key for OAP
+include 'keys.php';
+	//echo $appid;
+
 //Get specific variables to determine which function to call
 	$contact_id = $_GET["id"];
 	$rfm = $_GET['rfm'];
-
-function days_between1() {
 	$date_purchase = $_GET["datepurchase"];
 
 
+function write_log($logname) {
+	//Write the exact URL being pinged to stats_log file 
+		$filename = '/home/stats/public_html/kinostats/logs/'.$logname;
+		date_default_timezone_set('America/New_York');
+		$logDate = date('l jS \of F Y h:i:s A');
+		//$addr = "http://".$_SERVER['SERVER_NAME']."/".$_SERVER['REQUEST_URI']."?".$_SERVER['QUERY_STRING'];
+		$addr = "/".$_SERVER['REQUEST_URI'];
+		$write = $logDate." - ".$addr."\n";
+		file_put_contents ( $filename ,  $write , FILE_APPEND | LOCK_EX);
+}
 
+
+function days_between_1($contact_id, $date_purchase, $appid, $key) {
+	
+	//convert date to D-M-Y format
+		$pieces = explode("-", $date_purchase);	
+		$timestamp = $pieces[1].'-'.$pieces[0].'-'.$pieces[2];
+		$timestamp = strtotime($timestamp);
+		$now = time();
+		$diff = $now-$timestamp;
+		$diff = floor($diff/86400);
+		$diff_in_days = $diff;
+		$logname = 'days-between-1-2_log';
+
+	//echo 'diff in function = '.$diff_in_days.'<br>';
 
 $data = <<<STRING
 //You must pass the contact ID as an argument to indicate which contact is being updated
@@ -27,15 +58,12 @@ $data = <<<STRING
 </Group_Tag>
 </contact>
 STRING;
-}
 
-function api_call($data) {
-	// take the $data variable from the calculation function and make the API call
 	$data = urlencode(urlencode($data));
 
 	// Replace the strings with your API credentials located in Admin > OfficeAutoPilot API Instructions and Key Manager
-	$appid = "2_11369_H4sKVMEiL";
-	$key = "5VgmlzqqiY55TuI";
+	// $appid = "2_11369_H4sKVMEiL";
+	// $key = "5VgmlzqqiY55TuI";
 
 	$reqType= "update";
 	$postargs = "appid=".$appid."&key=".$key."&return_id=1&reqType=".$reqType. "&data=" . $data;
@@ -53,6 +81,21 @@ function api_call($data) {
 }
 
 
+
+function api_call($data) {
+	
+}
+
+
+if ($rfm = 1) {
+	days_between_1($contact_id, $date_purchase, $appid, $key);
+	
+	//api_call($data);
+}
+
+
+	//echo 'diff in days = '.$diff_in_days.'<br>';
+	//echo 'purchase date = '.$date_purchase.'<br>';
 
 
 ?>
